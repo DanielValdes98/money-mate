93bdb75edda6625107f03f511a216496c1adaf1f
Daniel Valdes <danielvaldes.dev@gmail.com>
2025-03-04 23:09:25 -0500

    Agregar componente AccordionFaqs y lógica para mostrar preguntas frecuentes; incluir estilos y animaciones en Tailwind

---
+++ frontend/app/(routes)/faqs/components/AccordionFaqs/AccordionFaqs.data.ts
@@ -0,0 +1,32 @@
+export const dataFaqs = [
+    {
+        id: 1,
+        question: "¿Qué es MoneyMate?",
+        answer: "MoneyMate es una plataforma que te ayuda a gestionar tu negocio de manera eficiente, proporcionando herramientas para el manejo de empresas, contactos, cotizaciones, pedidos y eventos."
+    },
+    {
+        id: 2,
+        question: "¿Cómo puedo registrar una empresa en MoneyMate?",
+        answer: "Para registrar una empresa, dirígete a la sección de 'Empresas' en el panel de usuario y selecciona 'Agregar Nueva Empresa'. Completa la información requerida y guarda los cambios."
+    },
+    {
+        id: 3,
+        question: "¿Cómo añado contactos a una empresa?",
+        answer: "Ve a la sección de 'Contactos' dentro del detalle de una empresa y haz clic en 'Agregar Contacto'. Ingresa el nombre, correo, teléfono y rol del contacto antes de guardarlo."
+    },
+    {
+        id: 4,
+        question: "¿Puedo gestionar cotizaciones desde el sistema?",
+        answer: "Sí, puedes crear, modificar y administrar cotizaciones en la ficha de cada cliente. La plataforma también permite convertir cotizaciones en pedidos de manera rápida."
+    },
+    {
+        id: 5,
+        question: "¿Cómo puedo visualizar y gestionar eventos?",
+        answer: "En la sección de 'Eventos', puedes ver todos los eventos asociados a tus empresas. Puedes crear nuevos eventos, editarlos o eliminarlos según sea necesario."
+    },
+    {
+        id: 6,
+        question: "¿Qué hacer si tengo problemas con la plataforma?",
+        answer: "Si experimentas problemas técnicos, puedes contactar al equipo de soporte enviando un mensaje a soporte@moneymate.com. También puedes consultar la sección de ayuda dentro de la plataforma."
+    }
+];
